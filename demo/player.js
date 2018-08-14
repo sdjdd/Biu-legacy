48a5085cec691e66032715767821cc01d9650bea
sdjdd <352207572@qq.com>
2018-08-14 13:05:34 +0800

    Update demo

--- demo/player.js
+++ demo/player.js
@@ -1,20 +1,40 @@
 var video = document.querySelector('.player video');
 var biu = new Biu({
-    el: '.danmaku'
+    el: '.danmaku',
+    speed: 7000,
+    style: {
+        opacity: 1,
+        fontSize: '25px'
+    }
 });
 
-danmakuData.forEach(function(v){
-    biu.load({
-        time: v.time,
-        text: v.text,
-        color: v.color,
-        type: v.type
-    });
-});
+biu.load(danmakuData);
+
+var seeked = false;
+var currentTime = 0;
+var divDanmaku = document.querySelector('.danmaku');
+var cbProtectEyes = document.querySelector('#cb-pe');
+cbProtectEyes.onchange = function(){
+    if (this.checked) {
+        divDanmaku.classList.add('cover');
+    } else {
+        divDanmaku.classList.remove('cover');
+    }
+}
+
+video.onseeked = function(){
+    seeked = true;
+    currentTime = video.currentTime;
+}
 
 video.onplay = function(){
-    let ms = Math.round(this.currentTime * 1000);
-    biu.play(ms);
+    if (seeked) {
+        seeked = false;
+        let ms = Math.round(currentTime * 1000);
+        biu.play(ms);
+    } else {
+        biu.play();
+    }
 }
 
 video.onpause = function(){
